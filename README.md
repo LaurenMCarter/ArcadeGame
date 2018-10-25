77c335fc50eb033d67bcdc0974d31a662e215669
LaurenMCarter <lmhorsey@gmail.com>
2018-10-25 09:58:54 -0400

    Final version

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 
 # Required files
 
-*index.html
-*js/app.js
-*js/engine.js
-*js/resources.js
-*images/char-boy.png
-*images/stone-block.png
-*images/water-block.png
-*images/grass-block.png
-*images/enemy-bug.png
+* index.html
+* js/app.js
+* js/engine.js
+* js/resources.js
+* images/char-boy.png
+* images/stone-block.png
+* images/water-block.png
+* images/grass-block.png
+* images/enemy-bug.png
 
 # Installation
 
 No install required. Simply open the index.html file in your favorite browser.
 
+# How to Play
+Move the player using the arrow keys.
+When player reaches the water row, the game is won.
+When player collides with a bug, player is reset to start position.
+
 # Special thanks to
 
-*Lloal Alas
-*Matthew Cranford
-*MDN web docs
+* Lloal Alas
+* Matthew Cranford
+* MDN web docs
--- js/app.js
+++ js/app.js
@@ -1,56 +1,102 @@
-
-var Enemy = function() {
+// Enemies our player must avoid
+var Enemy = function(x, y) {
     this.x = x;
-    thix.y = y;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
     this.height = 65;
     this.width = 95;
+    this.collision = false;
+    this.speed = 200;
 };
+
 
 Enemy.prototype.update = function(dt) {
-    //this.x += 150 * dt;
-
+//Thank you Lloan for this 
     if (this.x > ctx.canvas.width + this.width){
-        this.x = -200 * Math.floor(Math.random() * 4) + 1;
-    } else {
-        this.x += 150 * dt;
+        this.x = -300 * Math.floor(Math.random() * 4) + 1;
+    }   else {
+        this.x += this.speed * dt;
     }
-};
+    
+    //Adapted from the MDN web docs https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    if (player.x < this.x + this.width-35 &&
+        player.x + player.width > this.x &&
+        player.y < this.y + this.height &&
+        player.y + player.height > this.y) {
+            this.collision = true;
+            player.reset(202, 402);
+        }
+    };
 
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-const Player = function(x,y, sprite) {
+var Player = function(x, y){
+    this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
-    this.sprite = 'images/char-cat-girl.png';
     this.height = 75;
     this.width = 65;
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-Player.prototype.update = function(dt) {
-
-};
-
-Player.prototype.handleInput = function(direction){
-    const horizontal = 101;
-    const vertical = 83;
-
-    if (direction === 'left' && this.x - horizontal >= 0){
-        this.x -= 100;
-    } else if (direction === 'right' && this.x + horizontal < ctx.canvas.width ){
-        this.x += 100;
-    } else if (direction === 'down' && this.y + vertical < ctx.canvas.height - 200){
-        this.y -= 100;
-    } else if (direction === 'up' && thix.y - vertical > 0 - player.height){
-        this.y += 100;
-    }
+    this.positionX = 2;
+    this.positionY = 5;
 }
 
+Player.prototype.update = function(dt){
+    if (this.y <= -13 && this.positionY === 0){
+        this.winGame();
+    }    
+}; 
+
+//Show 'win' popup
+Player.prototype.winGame = function(){
+        window.alert("You win! Play again?");
+        this.reset(202, 402);
+};
+
+//Resets player to start position.
+Player.prototype.reset = function(x, y){
+    this.x = x;
+    this.y = y;
+    this.positionX = 2;
+    this.positionY = 5;
+}
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+//Handle user input to move character and keep character in bounds.
+Player.prototype.handleInput = function(direction){
+    switch(direction) {
+        case 'left':
+            if (this.positionX > 0) {
+                this.x -= 100;
+                this.positionX--;
+            }
+            break;
+        case 'right':
+            if (this.positionX < 4){
+                this.x += 100;
+                this.positionX ++;
+            } 
+            break;
+        case 'down':
+            if (this.positionY < 5){
+                this.y += 83;
+                this.positionY ++;
+                }
+            break;
+        case 'up':
+            if (this.positionY > 0){
+                this.y -= 83;
+                this.positionY --;
+            }
+            break;
+    };
+}
+
+//Handles user input for player movement
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -62,13 +108,12 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-
-
-
 const enemyPosition = [55, 140, 230];
 
-const allEnemies = enemyPosition.map((y,index) => {
-    return new Enemy( (-200 * (index + 1)), y);
-});
+const player = new Player(202, 402);
 
-const player = new Player();
+//Provided by Lloan. Randomized enemy position.
+const allEnemies = enemyPosition.map((y, index) => {
+    const x = -200 * (index + 1);
+    return new Enemy(x, y);
+});
